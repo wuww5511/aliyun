803cdab8c050dff629b99643a3c8ecaff5d0b6a6
wuww5511 <wuww5511@126.com>
2018-03-31 18:51:01 +0800

    记录访问时间

--- rule.js
+++ rule.js
@@ -5,7 +5,7 @@ module.exports.beforeSendRequest = function (requestDetail) {
     const ret = {}
 
 
-    console.log(`http: ${requestDetail._req.socket.remoteAddress}`)
+    console.log(`${'' + new Date()}  http: ${requestDetail._req.socket.remoteAddress}`)
     if (new RegExp('numenplus.yixin.im/neteaseattendance/attendance.do').test(requestDetail.url)) {
         let data = querystring.parse(requestDetail.requestData.toString())
         data.latitude = 'i-=m%+p%%-knvikqn'
@@ -17,6 +17,6 @@ module.exports.beforeSendRequest = function (requestDetail) {
 }
 
 module.exports.beforeDealHttpsRequest = function (requestDetail) {
-    console.log(`https: ${requestDetail._req.socket.remoteAddress}`)
+    console.log(`${'' + new Date()}  https: ${requestDetail._req.socket.remoteAddress}`)
     return Promise.resolve(false)
 }
